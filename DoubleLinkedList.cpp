f8d8bbebfff70db8cbdb7966db44383ba3104b94
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-05-15 11:06:41 +0700

    step 4

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -43,4 +43,6 @@ class DoubleLinkedList
                     cout << "\nDuplicate number not allowed" << endl;
                     return;
                 }
-           
+                
+            //step 4
+            newNode->next = START;

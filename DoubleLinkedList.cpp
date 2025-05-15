72b177e759c41d5ff6ac42f67e4c0baeebcaa68f
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-05-15 11:07:55 +0700

    add step 7

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -53,4 +53,8 @@ class DoubleLinkedList
 
             //step 6
             newNode->prev = NULL;
-           
+            
+            //step 7
+            START = newNode;
+            return;
+            } 

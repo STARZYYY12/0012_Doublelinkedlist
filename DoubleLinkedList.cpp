0e4f9813045fe3d954c5edee29f97daea94b84fb
STARZYYY12 <rizkyfaiz324@gmail.com>
2025-05-15 10:46:31 +0700

    doubleLinkedlist

--- DoubleLinkedList.cpp
+++ DoubleLinkedList.cpp
@@ -1,12 +1,31 @@
-#include
+#include <iostream>
 #include <string>
 using namespace std;
 
 class Node
 {
     public:
-    int NoMhs;
-    Node *next;
-    Node *prev;
-
+        int noMhs;
+        Node *next;
+        Node *prev;
 };
+
+class DoubleLinkedList
+{
+    private:
+        Node *START;
+
+    public:
+        DoubleLinkedList()
+        {
+            START = NULL;
+        }
+
+        void addNode()
+        {
+            int nim;
+            string nm;
+            cout << "\nEnter the roll number of the student";
+            cin >> nim;
+
+           
